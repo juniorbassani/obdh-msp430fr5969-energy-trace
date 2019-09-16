157d0fcb3b00d7524de9b4d75d8971e99bc194e5
juniorbassani <juba1099@hotmail.com>
2019-09-16 19:00:27 -0300

    Bugs corrected

--- small_csv/compare_tasks_consumption.sh
+++ small_csv/compare_tasks_consumption.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-# Read two arguments (csv files). Read same line of each file
-# and calculate the difference between the energy variation of each file
-# Store the results in a new file
+# Receives two arguments (csv files). Read the same line of each file,
+# calculate the difference between the energy variation of each file
+# and store the results in a new file
 
-# if [ $# != 3 ]
-    # exit -1
-# 
-lines=$(wc -l $1 | awk '{print $1}')
-# 
-# if [ $lines != $(wc -l $2 | awk '{print $1}') ]
-    # exit -1
+if [ $# != 2 ]; then
+    echo 'Exactly 2 CSV files need to be send to this script'
+    exit 1
+fi
+
+lines1=$(wc -l $1 | awk '{print $1}')
+lines2=$(wc -l $2 | awk '{print $1}')
+lines=0
+
+if [ $lines1 -le $lines2 ]; then
+    lines=$lines1
+else
+    lines=$lines2
+fi
 
 file1=$1
 file2=$2
 
-echo 'Time (ns),Variation (uJ)' > output
+echo 'Time (ns),Variation (uJ)' > output.csv
 
 # Loop begins in 2 to exclude header from being processed
 for (( i = 2; i <= $lines; i++ )); do
@@ -25,7 +32,7 @@ for (( i = 2; i <= $lines; i++ )); do
 
     time1=$(sed 's/"//g' <<< $(echo $file1_data | awk -F, '{print $1}'))
     time2=$(sed 's/"//g' <<< $(echo $file2_data | awk -F, '{print $1}'))
-    time_average=$(echo "($time1 + $time2) / 2" | bc -l)
+    time_average=$(echo "scale=2; ($time1 + $time2) / 2" | bc -l)
 
     # Only variation field
     file1_data=$(echo $file1_data | awk -F, '{print $5}')
@@ -36,8 +43,10 @@ for (( i = 2; i <= $lines; i++ )); do
 
     res=$(echo "$file1_data - $file2_data" | bc -l)
     
+    # Surround the data with double quotes
     time_average=$(sed "s/.*/\"&\",/" <<< $time_average)
     res=$(sed "s/.*/\"&\"/" <<< $res)
 
-    echo "${time_average}${res}" >> output
+    # Store the results in a CSV file
+    echo "${time_average}${res}" >> output.csv
 done
